c5cf572ae09fff357203c975ebe432e9f4b8639f
shogo1 <36905259+Shogo1@users.noreply.github.com>
2023-06-22 14:58:38 +0900

    名前変更

---
+++ python/tsumagari/def_2.py
@@ -0,0 +1,16 @@
+def say_somthing():
+    message = 'こんにちは、私は今西です'
+    print(message)
+
+
+say_somthing()
+
+masseage1 = 'こんばんは、私は今西です'
+print(masseage1)
+
+
+def say_somthing1():
+    print(masseage1)
+
+
+say_somthing1()
--- python/tsumagari/var.py
+++ python/tsumagari/var.py
@@ -1,16 +1,2 @@
-def say_somthing():
-    message = 'こんにちは、私は今西です'
-    print(message)
-
-
-say_somthing()
-
-masseage1 = 'こんばんは、私は今西です'
-print(masseage1)
-
-
-def say_somthing1():
-    print(masseage1)
-
-
-say_somthing1()
+a = 10
+print(a)
